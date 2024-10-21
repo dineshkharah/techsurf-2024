de8a9548225fa4782a8b2727cd41889612248634
Dinesh Kharah <dineshkharahcodes@gmail.com>
2024-10-21 21:21:44 +0530

    Refactor _document.js to load external scripts asynchronously using next/script

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
     return (
         <Html>
             <Head>
                 <link rel="stylesheet" href="https://ui.contentstack.com/contentstack.min.css" />
-                <script src="https://ui.contentstack.com/bootstrap.min.js" crossOrigin="anonymous"></script>
-                <script src="https://ui.contentstack.com/contentstack.min.js"></script>
             </Head>
             <body>
                 <Main />
                 <NextScript />
+
+                <Script
+                    src="https://ui.contentstack.com/bootstrap.min.js"
+                    crossOrigin="anonymous"
+                    strategy="lazyOnload"
+                />
+                <Script
+                    src="https://ui.contentstack.com/contentstack.min.js"
+                    strategy="lazyOnload"
+                />
             </body>
         </Html>
     );
